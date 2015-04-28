104348ca8e68a00faf096138c9ef54728bc83321
jesustomasdao <funambulo@hotmail.com>
2015-04-28 14:09:07 +0200

    update deprecated tools functions

--- tools/updateLatLongCentroide.php
+++ tools/updateLatLongCentroide.php
@@ -1,8 +1,9 @@
 <?php
 include "../db.php";
-
+//Script not active unless required
+exit();
 $link=connect();
 $sql="UPDATE lugares_shp SET xcentroid='{$_GET["xcentroid"]}', ycentroid='{$_GET["ycentroid"]}' WHERE id='{$_GET["id"]}'";
-mysql_query($sql,$link);
+mysqli_query($link,$sql);
 
 ?>
